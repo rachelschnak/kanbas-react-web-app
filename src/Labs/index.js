1ce395c4a832d53a7ebb63d2683bf3fe4787313c
rachelschnak <rachelschnak@gmail.com>
2023-11-02 16:19:11 -0400

    A4 Updates

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -12,7 +12,7 @@ function Labs() { return (
 
         <Nav/>
         <Routes>
-            <Route path="/" element={<Navigate to="a3"/>}/>
+            <Route path="/" element={<Navigate to="a4"/>}/>
             <Route path="a3" element={<Assignment3/>}/>
             <Route path="a4"
                    element={<Assignment4/>}/>
